766fae59a4d117d3735e8271540cd36282de0d01
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-22 12:51:35 +0530

    12th commit

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../provider/products_provider.dart';
+
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
   // final double price;
@@ -10,15 +11,43 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productId =
-        ModalRoute.of(context).settings.arguments as String; // here i get the data from Navigator and save in productId =>this is only id 
+    final productId = ModalRoute.of(context).settings.arguments
+        as String; // here i get the data from Navigator and save in productId =>this is only id
     final loadedProduct = Provider.of<Products>(
       context,
-      listen: false, //listen flase means i donot have intension to change ui after the app run first time
+      listen:
+          false, //listen flase means i donot have intension to change ui after the app run first time
     ).findById(productId);
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+              child: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Center(
+              child: Text(
+                loadedProduct.description,
+                // textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
