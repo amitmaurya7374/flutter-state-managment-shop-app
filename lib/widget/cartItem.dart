52e1a64117b66177364a2a9ae8b5c2aa7fbe3569
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-24 10:22:11 +0530

    alert dialog added

--- lib/widget/cartItem.dart
+++ lib/widget/cartItem.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../provider/cart.dart';
+
 class CartItem extends StatelessWidget {
   //I need some data for the widget to show some information
   final String id;
@@ -17,27 +18,61 @@ class CartItem extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    
     ///here now i want the swipe to delete functionality
     ///so i am goig to use dismissible widget
     ///it take a key for identify which card to delete
 
     return Dismissible(
       key: ValueKey(id),
-     direction: DismissDirection.endToStart,
+      direction: DismissDirection.endToStart,
       background: Container(
         color: Colors.red,
         child: Align(
           alignment: Alignment.centerRight,
           child: Padding(
             padding: const EdgeInsets.all(10.0),
-            child: Icon(Icons.delete,color: Colors.white,size:40.0),
+            child: Icon(Icons.delete, color: Colors.white, size: 40.0),
           ),
         ),
       ), //it show behindthat element once you start swipping
       //we use OnDismissed to permanetly delete the itemsfrom the cart
-      onDismissed: (direction){
-        Provider.of<Cart>(context,listen: false).removeItem(productId);
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      //creating a dialog box
+      confirmDismiss: (direction) {
+        //confirmDismiss return future
+        return showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('By Confirm this action you can delete that item'),
+                  actions: <Widget>[
+                    RaisedButton(
+                      elevation: 10.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)
+                      ),
+                      color: Colors.deepPurple,
+                      child: Text('Confirm',style: TextStyle(color: Colors.white),),
+                      onPressed: (){
+                        Navigator.of(context).pop(true);
+                      },
+                    ),
+                    RaisedButton(
+                      elevation: 10.0,
+                      color: Colors.deepPurple,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)
+                      ),
+                      child: Text('Cancel',style: TextStyle(color: Colors.white),),
+                      onPressed: (){
+                        Navigator.of(context).pop(false);
+                      },
+                    ),
+                  ],
+                ));
       },
       child: Card(
         margin: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 5),
