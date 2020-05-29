25dc03b388d635735ea1e7dafd8676c8a5889ff6
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-29 14:53:17 +0530

    34 commit

--- lib/provider/products_provider.dart
+++ lib/provider/products_provider.dart
@@ -166,9 +166,20 @@ class Products with ChangeNotifier {
     //place.
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+       final url = 'https://shopping-app-45175.firebaseio.com/products/$id.json';
+       //we use body here because we need to add some data
+       await  http.patch(url,body: json.encode(
+         {
+           'title':newProduct.title,
+           'description':newProduct.description,
+           'price':newProduct.price,
+           'imageUrl':newProduct.imageUrl,
+           
+         }
+       ));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
