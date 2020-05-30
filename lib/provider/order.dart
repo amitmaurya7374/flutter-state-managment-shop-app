98df75ace4f4deacd5f34c44d81cb5ff3ef46948
amitmaurya7374 <amitmaurya7374@gmail.com>
2020-05-30 10:36:20 +0530

    38 commit

--- lib/provider/order.dart
+++ lib/provider/order.dart
@@ -28,21 +28,55 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    const url = 'https://shopping-app-45175.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    print(json.decode(response.body));
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+   if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'] as double,//here is get the error
+          dateTime: DateTime.parse(orderData['dateTime']),
+          products: (orderData['products'] as List<dynamic>)
+              .map(
+                (item) => CartItem(
+                      id: item['id'],
+                      price: item['price'] as double,
+                      quantity: item['quantity'],
+                      title: item['title'],
+                    ),
+              )
+              .toList(),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url = 'https://shopping-app-45175.firebaseio.com/orders.json';
     final dateTimeStamp = DateTime.now();
- final response= await  http.patch(
+    final response = await http.patch(
       url,
       body: json.encode(
         {
-          'amount':total,
-          'dateTime':dateTimeStamp.toIso8601String(),
-          'product':cartProducts.map((cp)=>{
-            'id':cp.id,
-            'title':cp.title,
-            'quantity':cp.quantity,
-            'price':cp.price,
-          } ).toList(),
+          'amount': total,
+          'dateTime': dateTimeStamp.toIso8601String(),
+          'product': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
         },
       ),
     );
--- lib/screens/oder_screen.dart
+++ lib/screens/oder_screen.dart
@@ -6,8 +6,28 @@ import 'package:shop_state/widget/app_drawer.dart';
 import 'package:shop_state/widget/order_item.dart';
 import '../provider/order.dart' show Orders;
 
-class OrderScreen extends StatelessWidget {
+class OrderScreen extends StatefulWidget {
   static const routeName = '/order-page';
+
+  @override
+  _OrderScreenState createState() => _OrderScreenState();
+}
+
+class _OrderScreenState extends State<OrderScreen> {
+  var _isLoading = false;
+ @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_)async{
+      setState(() {
+        _isLoading = true;
+      });
+    await  Provider.of<Orders>(context,listen: false).fetchAndSetOrders();
+    setState(() {
+      _isLoading = false;
+      });
+    } );
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
